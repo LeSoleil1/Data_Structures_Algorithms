1673a5a97641bf29c2595ed5f2368b830c911b0c
soheilso <soheilso@gmail.com>
2020-05-05 23:54:16 -0400

    merged cpp files into h files

--- CPP/Linked_List_Based/Single_List/Single_node.h
+++ CPP/Linked_List_Based/Single_List/Single_node.h
@@ -68,5 +68,24 @@ class Single_node {
 		//    use   ptr->next()      to access it
 };
 
+template <typename Type>
+Single_node<Type>::Single_node( Type const &e, Single_node<Type> *n ):
+element( e ),
+next_node( n ) {
+	// empty constructor
+}
+
+template <typename Type>
+Type Single_node<Type>::retrieve() const {
+	return element;
+}
+
+template <typename Type>
+Single_node<Type> *Single_node<Type>::next() const {
+	return next_node;
+}
+
+// Is an error showing up in ece250.h or elsewhere?
+// Did you forget a closing '}' ?
 
 #endif
