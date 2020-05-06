2b333648d8afed90b60a0dca0c70bca781a8c431
soheilso <soheilso@gmail.com>
2020-05-06 01:55:45 -0400

    Modifying Dynamic_deque

--- CPP/Array_Based/Dynamic_Deque_Array/Dynamic_deque_as_array.h
+++ CPP/Array_Based/Dynamic_Deque_Array/Dynamic_deque_as_array.h
@@ -43,49 +43,42 @@ Dynamic_deque_as_array::Dynamic_deque_as_array(int n) :
 		array_size(std::max(1, n)),
 		array(new int[array_size]),
 		count(0) {
-		// empty
-		// C4 - you can choose to give initial values to ihead and itail here
-		// or  you can do it when you insert the first element
-
+		// empty 
 }
-
-
-
 
 Dynamic_deque_as_array::~Dynamic_deque_as_array() {
 	// The destructor deletes the memory allocated for the array.
 	delete[] array;	
 }
 
-
 int Dynamic_deque_as_array::size() const {
 	// Return the number of elements currently stored in the deque in O(1).
 	return count;
 }
-
 
 int Dynamic_deque_as_array::capacity() const {
 	// Return the capacity of deque in O(1).
 	return array_size;
 }
 
-
 bool Dynamic_deque_as_array::empty() const {
 	// Return true if deque is empty false otherwise in O(1).
 	return (count == 0);
 }
 
-
 int Dynamic_deque_as_array::head() const {
 	// Return the integer at the head of the data deque in O(1). It may throw an underflow exception.
-	if (empty()) throw underflow();
+	if (count == 0){
+		throw underflow();
+	} 
 	return array[ihead];
 }
 
-
 int Dynamic_deque_as_array::tail() const {
 	// Return the integer at the tail of the data deque in O(1). It may throw an underflow exception.
-	if (empty()) throw underflow();
+	if (count == 0){
+		throw underflow();
+	} 
 	return array[itail];
 }
 
@@ -101,16 +94,13 @@ int Dynamic_deque_as_array::wrap(int index) {
 
 }
 
-
-
-
 void Dynamic_deque_as_array::enqueue_head( const int &obj ) {
 	/* Insert a new item at the head of the deque in O(1). If the array
 	is full, create a new array with size 2n (where n is current size of the array) and copy the elements
 	from the current array to the new array in O(n).*/
 	if (size() == capacity()) {
 		int *old_Array;
-		old_Array= array;
+		old_Array = array;
 		int old_Size = size();
 		array_size = 2 * old_Size;
 		array = new int[array_size];
@@ -121,7 +111,7 @@ void Dynamic_deque_as_array::enqueue_head( const int &obj ) {
 		
 	}
 
-	if (empty()) {
+	if (count == 0) {
 		// when  inserting the first element, both head and tail are pointing to this element         
 		ihead = 0;
 		itail = 0;
@@ -139,7 +129,6 @@ void Dynamic_deque_as_array::enqueue_head( const int &obj ) {
 
 }
 
-
 void Dynamic_deque_as_array::enqueue_tail( const int &obj ) {
 	/* Insert a new item at the tail of the deque in O(1). If the array is
 	full, create a new array with size 2n (where n is current size of the array) and copy the elements
@@ -156,7 +145,7 @@ void Dynamic_deque_as_array::enqueue_tail( const int &obj ) {
 		delete[] old_Array;
 	}
 
-	if (empty()) {
+	if (count == 0) {
 		// when  inserting the first element, both head and tail are pointing to this element   
 		itail = 0;
 		ihead = 0;
@@ -171,15 +160,16 @@ void Dynamic_deque_as_array::enqueue_tail( const int &obj ) {
 
 	count++;
 	return;
-
 }
 
-
 int Dynamic_deque_as_array::dequeue_head() {
-	if (empty()) throw underflow();
+	// Remove the element at the head of the deque in O(1). This may throw an underflow exception.
+	if (count == 0){
+		throw underflow();
+	} 
 
 
-	int e_head = array[ihead];
+	int return_Value = array[ihead];
 
 	array[ihead] = 999999;// deleted marker for now
 
@@ -189,15 +179,18 @@ int Dynamic_deque_as_array::dequeue_head() {
 
 	count--;
 
-	return e_head;
+	return return_Value;
 
 
 }
 
 int Dynamic_deque_as_array::dequeue_tail() {
-	if (empty()) throw underflow();
+	// Remove the element at the tail of the deque in O(1). This may throw an underflow exception.
+	if (count == 0){
+		throw underflow();
+	} 
 
-	int e_tail = array[itail];
+	int return_Value = array[itail];
 
 	array[itail] = 999999;// deleted marker for now
 
@@ -207,30 +200,24 @@ int Dynamic_deque_as_array::dequeue_tail() {
 
 	count--;
 
-	return e_tail;
+	return return_Value;
 
 }
 
-
 void Dynamic_deque_as_array::clear() {
+	// Remove all elements from the deque in O(1).
 	count = 0;
 }
 
-
 void Dynamic_deque_as_array::print() {
 	cout << "---------------" << endl;
-
 	cout << "size(): " << size() << endl;
 	cout << "ihead: " << ihead << endl;
 	cout << "itail: " << itail << endl;
-
-
-
 	for (int i = 0; i < array_size; i++) {
 		cout << "i: " << i << " " << array[i] << endl;
 
 	}
-
 }
 
 #endif
