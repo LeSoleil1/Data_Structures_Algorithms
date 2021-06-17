cb002c5c095bb020946e65de022ce36f18737256
Soheil <soheilso@gmail.com>
2021-06-17 17:28:29 -0400

    Tested in 2D

--- CPP/Tree_Based/Point_Region_quadtree/qtree.cpp
+++ CPP/Tree_Based/Point_Region_quadtree/qtree.cpp
@@ -4,10 +4,12 @@
 #include <random>
 #include <math.h>
 #include <tuple>
+#include <chrono>
 
 #include <GL/glut.h>
 
 using namespace std;
+using namespace std::chrono;
 
 class Point
 {
@@ -191,7 +193,7 @@ public:
 
 ostream &operator<<(ostream &out, const Rectangle &rec)
 {
-	out << rec.x << '\t' << rec.y << '\t' << rec.w << '\t' << rec.h << std::endl;
+	out << "Center_x: " << rec.x << '\t' << "Center_y: " << rec.y << '\t' << "Range_x: " << rec.w << '\t' << "Range_y: " << rec.h << std::endl;
 	return out;
 }
 
@@ -212,7 +214,7 @@ class QuadTree
 	Rectangle boundary;
 	int capacity;
 	std::vector<Point> points;
-	bool devided = false;
+	bool devided;
 
 	QuadTree *northwest;
 	QuadTree *northeast;
@@ -220,18 +222,42 @@ class QuadTree
 	QuadTree *southeast;
 
 public:
-	QuadTree(Rectangle inp_boundary, int n)
+	static std::vector<Rectangle> boundaries;
+
+	QuadTree(Rectangle inp_boundary, int inp_capacity)
 	{
-		boundary = inp_boundary;
-		capacity = n;
-		northwest = nullptr;
-		northeast = nullptr;
-		southwest = nullptr;
-		southeast = nullptr;
+		if (inp_capacity < 1)
+		{
+			throw std::invalid_argument("received capacity less than 1");
+		}
+		this->devided = false;
+		this->boundary = inp_boundary;
+		this->capacity = inp_capacity;
+		this->points = std::vector<Point>();
+
+		this->northwest = nullptr;
+		this->northeast = nullptr;
+		this->southwest = nullptr;
+		this->southeast = nullptr;
 	}
+	std::vector<QuadTree *> get_children()
+	{
+		if (this->northeast != nullptr)
+		{
+			std::vector<QuadTree *> ret = {this->northeast, this->northwest, this->southeast, this->southwest};
+			return ret;
+		}
+		std::vector<QuadTree *> ret = {nullptr, nullptr, nullptr, nullptr};
+		return ret;
+	}
+
 	Rectangle get_boundary()
 	{
 		return boundary;
+	}
+	vector<Rectangle> get_boundaries()
+	{
+		return boundaries;
 	}
 	void subdevide()
 	{
@@ -240,38 +266,41 @@ public:
 		int w = this->boundary.get_w();
 		int h = this->boundary.get_h();
 
-		Rectangle ne = Rectangle(x + w / 4, y - h / 4, w / 2, h / 2);
-		Rectangle nw = Rectangle(x - w / 4, y - h / 4, w / 2, h / 2);
-		Rectangle se = Rectangle(x + w / 4, y + h / 4, w / 2, h / 2);
-		Rectangle sw = Rectangle(x - w / 4, y + h / 4, w / 2, h / 2);
+		Rectangle ne = Rectangle(x + w / 4, y - h / 4, w / 2, h / 2); //CHECK
+		Rectangle nw = Rectangle(x - w / 4, y - h / 4, w / 2, h / 2); //CHECK
+		Rectangle se = Rectangle(x + w / 4, y + h / 4, w / 2, h / 2); //CHECK
+		Rectangle sw = Rectangle(x - w / 4, y + h / 4, w / 2, h / 2); //CHECK
 
-		this->northwest = new QuadTree(nw, this->capacity);
 		this->northeast = new QuadTree(ne, this->capacity);
-		this->southwest = new QuadTree(sw, this->capacity);
+		this->northwest = new QuadTree(nw, this->capacity);
 		this->southeast = new QuadTree(se, this->capacity);
+		this->southwest = new QuadTree(sw, this->capacity);
 
 		this->devided = true;
 		// cout << "Devided" << std::endl;
 	}
+
 	bool insert(Point point)
 	{
 		if (!this->boundary.contains(point))
 		{
 			return false;
 		}
-		if (this->points.size() < this->capacity)
+		if (this->points.size() < this->capacity) //CHECK
 		{
+			// cout << this->boundary << std::endl;
+			// cout << "This boundary has capacity " << this->boundary;
 			this->points.push_back(point);
 			return true;
 		}
-		else
+		else if (!this->devided)
 		{
-			if (!this->devided)
-			{
-				cout << this->boundary;
-				this->subdevide();
-			}
+			// cout << this->boundary << std::endl;
+			// cout << "This boundary doesn't have capacity!!! " << this->boundary;
+			boundaries.push_back(boundary);
+			this->subdevide();
 		}
+
 		return (
 			this->northeast->insert(point) ||
 			this->northwest->insert(point) ||
@@ -303,6 +332,25 @@ public:
 		}
 		return found;
 	}
+	// Temporary
+	void print_Boundaries()
+	{
+		if (this->devided)
+		{
+			this->northwest->print_Boundaries();
+			this->northeast->print_Boundaries();
+			this->southwest->print_Boundaries();
+			this->southeast->print_Boundaries();
+		}
+		else
+		{
+			if (this->points.size() > this->capacity){
+				cout << this->boundary;
+				cout << this->points.size() << std::endl;
+			}
+
+		}
+	}
 	// TODO K nearest neighbor
 };
 
@@ -313,104 +361,164 @@ constexpr int CAPACITY = 4;
 constexpr int center_x = 200;
 constexpr int center_y = 200;
 
-void display1(void)
-{
+// void display1(void)
+// {
 
-	// Line x axis
-	glColor3ub(254, 0, 0);
-	glBegin(GL_LINES);
-	glVertex3f(-1.0, 0.0, 0.0);
-	glVertex3f(1.0, 0.0, 0.0);
-	glEnd();
+// 	// Line x axis
+// 	glColor3ub(254, 0, 0);
+// 	glBegin(GL_LINES);
+// 	glVertex2f(-1.0, 0.0);
+// 	glVertex2f(1.0, 0.0);
+// 	glEnd();
 
-	// Line y axis
-	glColor3ub(31, 255, 0);
-	glBegin(GL_LINES);
-	glVertex3f(0.0, -1.0, 0.0);
-	glVertex3f(0.0, 1.0, 0.0);
-	glEnd();
+// 	// Line y axis
+// 	glColor3ub(31, 255, 0);
+// 	glBegin(GL_LINES);
+// 	glVertex2f(0.0, -1.0);
+// 	glVertex2f(0.0, 1.0);
+// 	glEnd();
 
-	glFlush();
-}
-
+// 	glFlush();
+// }
+std::vector<Rectangle> QuadTree::boundaries = std::vector<Rectangle>();
 
 int main(int argc, char **argv)
 {
+	auto mean_x = center_x;
+	auto mean_y = center_y;
+	auto stddev_x = WIDTH / 8;
+	auto stddev_y = HEIGHT / 8;
 
 	std::random_device rd;
 	std::mt19937 e2(rd());
-	std::normal_distribution<> dist_width(center_x, WIDTH / 8);
-	std::normal_distribution<> dist_height(center_y, HEIGHT / 8);
+	std::normal_distribution<> dist_width(mean_x, stddev_x);
+	std::normal_distribution<> dist_height(mean_y, stddev_y);
 
 	Rectangle boundary(center_x, center_y, WIDTH, WIDTH);
-	// Point p(10,15);
-	// cout << boundary.contains(p);
 	QuadTree qt(boundary, CAPACITY);
 	int counter = 0;
+	int x_int;
+	int y_int;
+	auto start = chrono::high_resolution_clock::now();
+	for (int i = 0; i < 70000; i++)
+	{
+		x_int = round(dist_width(e2));
+		y_int = round(dist_height(e2));
+		Point p(x_int, y_int);
+		// Point p(rand() % WIDTH, rand() % HEIGHT);
+		// cout << p;
+		bool res = qt.insert(p);
+		// if (res == 1)
+		// {
+		// 	counter++;
+		// }
+		// cout << "inserted\t" << res << std::endl;
+		// cout << boundary.contains(p) << std::endl;
+	}
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+  
+	// To get the value of duration use the count()
+	// member function on the duration object
+	cout << "Generating the tree takes " << duration.count() << " microseconds" << std::endl;
+
+	qt.print_Boundaries();
+
+	vector<Rectangle> boundaries = qt.get_boundaries();
+
+	// cout << " Number of points inserted: " << counter << std::endl;
+	// cout << qt.get_boundary();
+	Rectangle range(200, 200, 200, 200);
+	vector<Point> points;
+	points = qt.query(range, points);
+	cout << points.size() << " Number of points in the: "  << "range: " << range << std::endl;
+
+	// // PRINT ALL THE BOUNDARIES
+	// cout << '\n'
+	// 	 << "BOUNDARIES:" << '\n';
+	// for (auto boundary : boundaries)
+	// {
+	// 	cout << boundary << std::endl;
+	// }
+
+	// // PRINT ALL THE POINTS
+	// cout << '\n'
+	// 	 << "POINTS:" << '\n';
+	// for (auto point : points)
+	// {
+	// 	cout << point << ' ';
+	// }
 
 	// Drawing ///////////////
 	glutInit(&argc, argv);
-	glutInitWindowSize(1000, 800);
+	glutInitWindowSize(1800, 1800);
 	glutCreateWindow("Test");
 	glClearColor(1.0, 1.0, 1.0, 1.0); // Background color
 	float x;
 	float y;
-		glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT);
+	// counter = 0;
 
-	/////////////////////////
-
-	for (int i = 0; i < 50; i++)
+	// Plotting Boundaries
+	// int x_int;
+	// int y_int;
+	int w_int;
+	int h_int;
+	for (auto boundary : boundaries)
 	{
-		int x_int = round(dist_width(e2));
-		int y_int = round(dist_height(e2));
-		Point p(x_int, y_int);
-		//////////////////////////////////
-		x = (x_int- center_x) / (WIDTH/2.0);   
-		y = (y_int- center_y) / (HEIGHT/2.0); 
+		x_int = boundary.get_x();
+		y_int = boundary.get_y();
+		w_int = boundary.get_w();
+		h_int = boundary.get_h();
+		float x_start = x_int - w_int / 2.0;
+		float x_stop = x_int + w_int / 2.0;
+		float y_start = y_int - h_int / 2.0;
+		float y_stop = y_int + h_int / 2.0;
+		x_start = (x_start - center_x) / (WIDTH / 2.0);
+		x_stop = (x_stop - center_x) / (WIDTH / 2.0);
+		x = (x_int - center_x) / (WIDTH / 2.0);
+		y_start = (y_start - center_y) / (HEIGHT / 2.0);
+		y_stop = (y_stop - center_y) / (HEIGHT / 2.0);
+		y = (y_int - center_y) / (HEIGHT / 2.0);
+		// cout << x_start << '\t' << x_stop << '\t' << y << std::endl;
+		// cout << y_start << '\t' << y_stop << '\t' << x << std::endl;
+
+		// Line x axis
+		glColor3ub(254, 0, 0);
+		glBegin(GL_LINES);
+		glVertex2f(x_start, y);
+		glVertex2f(x_stop, y);
+		glEnd();
+
+		// Line y axis
+		glColor3ub(31, 255, 0);
+		glBegin(GL_LINES);
+		glVertex2f(x, y_start);
+		glVertex2f(x, y_stop);
+		glEnd();
+
+		glFlush();
+	}
+
+	// Plotting points
+	for (auto point : points)
+	{
+		counter++;
+		x_int = point.get_x();
+		y_int = point.get_y();
+		x = (x_int - center_x) / (WIDTH / 2.0);
+		y = (y_int - center_y) / (HEIGHT / 2.0);
 		glPointSize(3);
 		glBegin(GL_POINTS);
 		glColor3f(0.5, 0, 0);
 		glVertex2f(x, y);
 		glEnd();
 		glFlush();
-		/////////////////////////////////
-
-		// Point p(rand() % WIDTH, rand() % HEIGHT);
-		// cout << p;
-		bool res = qt.insert(p);
-		if (res == 1)
-		{
-			counter++;
-			// cout << p;
-		}
-		// cout << "inserted\t" << res << std::endl;
-		// cout << boundary.contains(p) << std::endl;
 	}
-	///////////////////////////////////////
-	glutDisplayFunc(display1);
+
+	// cout << counter;
+	// glutDisplayFunc(display1);
 	glutMainLoop();
 	//////////////////////////////////////
-	cout << counter << std::endl;
-	// cout << qt.get_boundary();
-	Rectangle range(200, 200, 100, 100);
-	vector<Point> points;
-	points = qt.query(range, points);
-	cout << points.size() << std::endl;
-	// for (auto point : points)
-	// {
-	// 	cout << point << ' ';
-	// }
-
-	// std::random_device rd;
-	// std::mt19937 e2(rd());
-	// std::normal_distribution<> dist(200,200);
-	// vector<int> p;
-	// for (int i = 0; i <100 ; i++){
-	// 	p.push_back(round(dist(e2)));
-	// }
-	// for (auto i : p){
-	// 	std::cout << i << ' ';
-	// }
-	// cout<<p.size();
 	return 0;
 }
